c360a8e0dbd1b7d1a7af1a81ad0c121422f3322d
srg206 <you@example.com>
2025-03-16 18:53:28 +0300

    fixes to be able to deploy on cluster

--- settings/settings.py
+++ settings/settings.py
@@ -6,11 +6,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Postgres(BaseModel):
-    database: str = "db_main"
-    host: str = "localhost"
+    database: str = ""
+    host: str = ""
     port: int = 5432
-    username: str = "db_main"
-    password: str = "db_main"
+    username: str = ""
+    password: str = ""
 
 
 class Uvicorn(BaseModel):
@@ -23,7 +23,7 @@ class _Settings(BaseSettings):
     pg: Postgres = Postgres()
     uvicorn: Uvicorn = Uvicorn()
 
-    model_config = SettingsConfigDict(env_file=".env", env_prefix="app_", env_nested_delimiter="__")
+    model_config = SettingsConfigDict(env_prefix="app_", env_nested_delimiter="__")
 
 
 settings = _Settings()
